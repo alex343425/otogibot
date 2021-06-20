9c32e6f43d6756f63842741881c3f9b5ba125693
alex343425 <38518603+alex343425@users.noreply.github.com>
2021-06-20 14:21:15 +0800

    999

---
+++ idparse.py
@@ -0,0 +1,211 @@
+def setskilltype(w):
+    if w == 'i':
+        return('拆技')
+    elif w == 'a':
+        return('昇華')
+    elif w == 't':
+        return('昇華變化')
+    elif w == 's':
+        return('昇華超變化')
+    elif w == 'u':
+        return('限突')
+    elif w == 'v':
+        return('限突變化')
+    elif w == 'x':
+        return('限突超變化')
+    elif w == 'w':
+        return('專武技能')
+    elif w == 'l':
+        return('隊長技')
+    elif w == 'n':
+        return('非專武武器技能')
+    elif w == 'r':
+        return('精靈技能')
+    elif w == 'o':
+        return('未能確定來源技能')
+    else:
+        return('不明技能')
+
+
+def attribute(a):
+    if a == 1:
+        return('火')
+    elif a == 2:
+        return('水')
+    elif a == 3:
+        return('樹')
+    elif a == 4:
+        return('光')
+    elif a == 5:
+        return('闇')
+    else:
+        return('')
+
+def weaponclass(wc):
+    if wc == 1:
+        return('劍')
+    elif wc == 2:
+        return('斧')
+    elif wc == 3:
+        return('槍')
+    elif wc == 4:
+        return('本')
+    elif wc == 5:
+        return('杖')
+    elif wc == 6:
+        return('短劍')
+    elif wc == 7:
+        return('弓')
+    elif wc == 8:
+        return('特殊')
+    else:
+        return('')
+
+def skillclass(sc):
+    if sc == 1:
+        return('橘')
+    elif sc == 2:
+        return('藍')
+    elif sc == 3:
+        return('紫')
+    elif sc == 4:
+        return('粉')
+    elif sc == 5:
+        return('綠')
+    else:
+        return('')
+
+def skillrank(sr):
+    if sr == 2:
+        return('D')
+    elif sr == 3:
+        return('C')
+    elif sr == 4:
+        return('B')
+    elif sr == 5:
+        return('A')
+    elif sr == 6:
+        return('S')
+    elif sr == 7:
+        return('SS')
+    elif sr == 8:
+        return('S3')
+    else:
+        return('')
+
+def isinor(kw,sentence):
+    for k in kw:
+        if k.startswith('-'):
+            if k[1:] in sentence.lower():
+                return False
+    for k in kw:
+        if k in sentence.lower():
+            return True
+    return False
+
+def isinand(kw,sentence):
+    for k in kw:
+        if k.startswith('-'):
+            if k[1:] in sentence.lower():
+                return False
+        else:
+            if k not in sentence.lower():
+                return False
+    return True
+
+def isinforward(kw,sentence):
+    n = 0
+    for k in kw:
+        if k.startswith('-'):
+            if sentence.lower().find(k[1:],n) != -1:
+                return False
+        else:
+            n = sentence.lower().find(k,n)+1
+            if n == 0:
+                return False
+    return True
+
+def isinbackward(kw,sentence):
+    n = len(sentence.lower())-1
+    for k in kw:
+        if k.startswith('-'):
+            if sentence.lower().find(k[1:],0,n) != -1:
+                return False
+        else:
+            n = sentence.lower().find(k,0,n)
+            if n == -1:
+                return False
+    return True
+
+def isininterval(kw,sentence):
+    lb = kw[0]
+    ub = kw[1]
+    kw = kw[2:]
+    
+    if lb == '-':
+        lbn = 0
+    else:
+        lbn = sentence.lower().find(lb)
+        
+    if lbn == -1:
+        return False
+    
+    n = len(sentence.lower())-1
+    
+    if ub == '-':
+        ubn = n
+    else:
+        ubn = sentence.lower().find(ub,lbn)
+        
+    if ubn == -1:
+        return False
+        
+    if lbn >= ubn:
+        return False
+    
+    for k in kw:
+        if k.startswith('-'):
+            if sentence.lower().find(k[1:],lbn,ubn) != -1:
+                return False
+        else:
+            if sentence.lower().find(k,lbn,ubn) == -1:
+                return False
+    return True
+
+def isindamage(kw,sentence):
+    try:
+        threshold = int(kw[0])
+    except:
+        return False
+    if not isinand(kw[1:],sentence.lower()):
+        return False
+    
+    location = sentence.lower().find('威力')
+    while location != -1:
+        damage = ''
+        for x in sentence.lower()[location+2:]:
+            if x.isdigit():
+                damage += x
+            else:
+                break
+        if damage != '' and int(damage) >= threshold:
+            return True
+        else:
+            location  = sentence.lower().find('威力',location+2)
+    return False
+    
+def check_sc(sc,cate):
+    if str(sc-1) in cate:
+        return True
+    else:
+        pass
+    return False
+
+def check_sa(a,cate):
+    if a == 6:
+        return False
+    elif str(a+4) in cate:
+        return True
+    else:
+        pass
+    return False
---
+++ main.py
@@ -0,0 +1,88 @@
+import os
+import discord
+import asyncio
+from botUI import utilityUI, funUI, privateUI, publicUI
+from checkupdate import checkupdate
+from nick import loadnick, loadsp
+from parseskills import skillsourcecate, updatemfiles
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+guild_ids = {855879876815618078, # Test Server
+             624974729689694228  # Main Server
+            }
+
+private_message_ids = {649153350431932437, # Scarlet
+                       361903872270270465, # 卡比
+                       554311206119145472, # Be
+                       268293880108023808, # 海苔
+                       470123343568175106, # T佬
+                       853885325000835072 # Bu
+                       }
+
+utilityUICommandList = {'!ping'
+                        }
+
+funUICommandList = {'?rotate'
+                    }
+
+privateUICommandList = {'?getcg',
+                        '?inspect',
+                        '?beid',
+                        '?loadnick',
+                        '?loadsp',
+                        '?update',
+                        '?labyrinth'
+                        }
+
+publicUICommandList = {'?char',
+                       '?nick',
+                       '?spirit',
+                       '?be',
+                       '!story',
+                       '!event'
+                       }
+
+loadnick()
+loadsp()
+updatemfiles()
+skillsourcecate()
+
+@bot.event
+async def on_ready():
+    await checkupdate(bot)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if message.guild.id in guild_ids:
+        pass
+    else:
+        return
+      
+    if message.content.split(' ')[0] in funUICommandList:
+        await funUI(message,bot)
+        return
+    
+    if message.content.split(' ')[0] in utilityUICommandList:
+        await utilityUI(message,bot)
+        return
+  
+    if message.content.split(' ')[0] in privateUICommandList:
+        if message.author.id in private_message_ids:
+            pass
+        else:
+            return
+        await privateUI(message,bot)
+        return
+    
+    if message.content.split(' ')[0] in publicUICommandList or message.content.startswith('?skill') or message.content.startswith('?story') or message.content.startswith('?event'):
+        await publicUI(message,bot)
+        return
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
