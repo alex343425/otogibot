e60fa11c7966dff344a9af837845d5c69a5db15b
alex343425 <38518603+alex343425@users.noreply.github.com>
2023-04-09 23:40:27 +0800

    Update botUI.py

--- bot/botUI.py
+++ bot/botUI.py
@@ -1576,6 +1576,7 @@ async def publicUI_kirby(message,bot):
         return Image.open(BytesIO(requests.get(img_url).content))
     reminder_channel = bot.get_channel(626708913257185280)
     starting_channel = bot.get_channel(855880177224253440)
+    reminder_channel_alt = bot.get_channel(624974729689694230)
     if message.content.lower().startswith('?公告'):
         news_latest_check = requests.get(cfg.addresslatest, headers={'token': cfg.token_jp}).json()
         keyword = message.content.split(' ')[1]
